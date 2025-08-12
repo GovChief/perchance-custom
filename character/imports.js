50fc386dabfd73c30a70e96a91196aac565788f1
Jakov <48694899+GovChief@users.noreply.github.com>
2025-08-12 21:59:01 +0200

    Update imports.js (#11)

--- character/imports.js
+++ character/imports.js
@@ -2,8 +2,9 @@ const repoPath = 'https://cdn.jsdelivr.net/gh/GovChief/perchance-custom@main/cha
 
 const debug = await import(`${repoPath}/debug/debug.js`);
 const messageProcessing = await import(`${repoPath}/processing/messageProcessing.js`);
-if (!debug || !messageProcessing) {
-  throw new Error("Failed to load required modules: debug and/or messageProcessing.");
+const html = await import(`${repoPath}/ui/html.js`);
+if (!debug || !messageProcessing || !html) {
+  throw new Error("Failed to load required modules: debug, messageProcessing and/or html.");
 }
 
-export { debug, messageProcessing };
+export { debug, messageProcessing, html };
