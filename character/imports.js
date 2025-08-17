ec9235e904174e38ceab46c139604f30b4a6291f
Jakov <48694899+GovChief@users.noreply.github.com>
2025-08-17 17:23:18 +0200

    Update imports.js

--- character/imports.js
+++ character/imports.js
@@ -32,7 +32,7 @@ try {
 }
 
 if (failedModules.length > 0) {
-  throw new Error("Imports failed to load required modules: " + failedModules.join(', ') + ".");
+  throw new Error("Imports module failed to load required modules: " + failedModules.join(', ') + ".");
 }
 
 export { debug, messageProcessing, ui, globals };
